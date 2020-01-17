cbf2a032f82f0be7c371f6969041a6ad5c59c237
Phillip Shreves <pshreves@highpointgo.com>
2020-01-17 12:45:38 -0600

    Added objects.js and formatter.js

---
+++ src/main/formatter.js
@@ -0,0 +1,12 @@
+
+function character_separator(character) {
+    this.character = character;
+    /* Check for:
+        parenthesis
+        square brackets
+        quotes
+        semicolons
+        ampersands
+        math operators
+    */
+}
