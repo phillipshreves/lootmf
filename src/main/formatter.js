01768ec9e34c8464c9768d2817890422896c5a66
Phillip Shreves <pshreves@highpointgo.com>
2020-01-31 12:07:05 -0600

    Added reserved characters

--- src/main/formatter.js
+++ src/main/formatter.js
@@ -10,3 +10,13 @@ function character_separator(character) {
         math operators
     */
 }
+
+funtion character_reserved(character) {
+    this.character = character;
+    /*
+        contains: + - * / ^ & = ≠ < > ≤ ≥ ( ) , ; [ ] " :: $ }
+        contains: AND, OR, XOR, NOT
+        begins with a digit or a period
+        has the same name as a function, parameter or keyword
+    */
+}
