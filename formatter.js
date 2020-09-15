8859d4c40499226b4096bd9423420bfc663a6850
Phillip Shreves <hi@phillips.work>
2020-09-14 20:19:01 -0500

    Implemented Javascript button functionality to update DOM. Started building out formatter script.

--- formatter.js
+++ formatter.js
@@ -1,3 +1,29 @@
-function formatter(input_text){
-    console.log("succes")
+function formatter(input_text) {
+    let output_text = '';
+    let indent_level = 0;
+    let tab = '\t';
+
+    //Clean up whitespace. Replace carraige returns, then set all whitespace to spaces, then replace consecutive spaces with one space. This helps the parser determine what sort of object it is handling 
+    input_text = input_text.replace(/(?:\r\n|\r|\n)/g, ' <<<newline>>> ').replace(/\s/g, ' ').replace(/ +/g, ' ').split(' ');
+
+    for (character of input_text) {
+        let new_text = function(character){
+            switch (character) {
+                case '(' || '[':
+                    indent_level++; 
+                    return character + '<<<newline>>>' + tab.repeat(indent_level);
+                    break;
+                
+                default:
+                    return character
+            }
+        };
+        output_text = output_text + new_text(character)
+    }
+
+    //Add carriage returns back
+    output_text = output_text.replace(/<<<newline>>>/g, '\r')
+
+    // Update the DOM
+    document.getElementById('output_text').innerHTML = output_text
 };
