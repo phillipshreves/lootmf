1364d5f01cdabe1737d7a206ecd1eb096bee4e1f
Phillip Shreves <hi@phillips.work>
2020-10-02 12:10:43 -0500

    Added handling for paranthesis and brackets

--- formatter.js
+++ formatter.js
@@ -2,12 +2,11 @@ function formatter(input_text) {
     let output_text = '';
     let indent_level = 0;
     let tab = '\t';
-
-    //Clean up whitespace. Replace carraige returns, then set all whitespace to spaces, then replace consecutive spaces with one space. This helps the parser determine what sort of object it is handling 
-    input_text = input_text.replace(/(?:\r\n|\r|\n)/g, '\n');
-    console.log(input_text)
-
     let converted_comment_text = ''
+
+    //Clean up whitespace by setting all carriage returns to newline characters
+    input_text = input_text.replace(/(?:\r\n|\r|\n)/g, '\n');
+
     for (let full_text_index=0 ; full_text_index < input_text.length ; full_text_index++) {
         let character = input_text[full_text_index];
         let character_next = input_text[full_text_index+1];
@@ -28,28 +27,49 @@ function formatter(input_text) {
     }
     input_text = converted_comment_text;
 
-    //Clean up whitespace. Replace carraige returns, then set all whitespace to spaces, then replace consecutive spaces with one space. This helps the parser determine what sort of object it is handling 
-    input_text = input_text.replace(/\s/g, ' ').replace(/ +/g, ' ').split(' ');
+    //Clean up whitespace. Set all whitespace to spaces, then replace consecutive spaces with one space, then split into an array. This helps the parser determine what sort of object it is handling 
+    input_text = input_text.replace(/\[/g, ' [ ').replace(/\]/g, ' ] ').replace(/\(/g, ' ( ').replace(/\)/g, ' ) ').replace(/\s/g, ' ').replace(/ +/g, ' ').split(' ');
 
-    console.log(input_text)
+    for (object of input_text) {
+        let new_text = function(object){
 
-    for (character of input_text) {
-        let new_text = function(character){
-            switch (character) {
-                case '(' || '[':
+            // indent level control
+            switch (object) {
+                case '(':
+                case '[':
                     indent_level++; 
-                    return character + '\n' + tab.repeat(indent_level);
+                    break;
+
+                case ')':
+                case ']':
+                    indent_level--;
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Add to the text
+            switch (object) {
+                case '(':
+                case '[':
+                    return object + '\n' + tab.repeat(indent_level);
                     break;
                 
+                case ')':
+                case ']':
+                    return '\n' + tab.repeat(indent_level) + object;
+                    break;
+
                 default:
-                    return character
+                    return object;
             }
         };
-        output_text = output_text + new_text(character)
+        output_text = output_text + new_text(object)
     }
 
     //Add carriage returns back
-    output_text = output_text.replace(/<<end_comment>>/g, '\r')
+    output_text = output_text.replace(/<<end_comment>>/g, '\n')
 
     // Update the DOM
     document.getElementById('output_text').innerHTML = output_text
