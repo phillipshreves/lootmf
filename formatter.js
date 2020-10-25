77314bb6177a65dfe040db7a61c49b49dc0717f2
Phillip Shreves <hi@phillips.work>
2020-10-25 14:55:04 -0500

    Added formatting for semicolons. Added formatting for strings

--- formatter.js
+++ formatter.js
@@ -2,15 +2,28 @@ function formatter(input_text) {
     let output_text = '';
     let indent_level = 0;
     let tab = '\t';
-    let converted_comment_text = ''
+    let converted_comment_text = '';
+    let parsing_string = false;
+
 
     //Clean up whitespace by setting all carriage returns to newline characters
     input_text = input_text.replace(/(?:\r\n|\r|\n)/g, '\n');
 
+
+    //This loop applies preformatting to help the parser determine what kind of objects it's dealing with
     for (let full_text_index=0 ; full_text_index < input_text.length ; full_text_index++) {
         let character = input_text[full_text_index];
         let character_next = input_text[full_text_index+1];
-        if (character == '/' && character_next == '/') {
+
+        if(character == '"') {
+            parsing_string = !parsing_string
+        }
+
+        if(parsing_string) {
+            converted_comment_text = converted_comment_text + character
+
+        //Here we handle single line comments, which are a little tricky to parse, so we add some custom formatting and then clean it up at the end of the script
+        } else if (character == '/' && character_next == '/') {
             for(let comment_index=full_text_index ; comment_index < input_text.length ; comment_index++){
                 let converted_comment_character = input_text[comment_index]
                 if ( converted_comment_character == '\n') {
@@ -21,55 +34,73 @@ function formatter(input_text) {
                 };
                 full_text_index++
             }
+        } else if(character == ';') {
+            converted_comment_text = converted_comment_text + ' ' + character + ' '
         } else {
             converted_comment_text = converted_comment_text + character
         }
     }
     input_text = converted_comment_text;
 
+
     //Clean up whitespace. Set all whitespace to spaces, then replace consecutive spaces with one space, then split into an array. This helps the parser determine what sort of object it is handling 
     input_text = input_text.replace(/\[/g, ' [ ').replace(/\]/g, ' ] ').replace(/\(/g, ' ( ').replace(/\)/g, ' ) ').replace(/\s/g, ' ').replace(/ +/g, ' ').split(' ');
 
-    for (object of input_text) {
+
+    parsing_string = false;
+    //This loop handles each object and applies formatting
+    for (let object_counter=0 ; object_counter < input_text.length ; object_counter++) {
+        let object = input_text[object_counter];
+
+        if(object == '"') {
+            parsing_string = !parsing_string
+        }
+
         let new_text = function(object){
+            if(parsing_string) {
+                return object
+            } else {
+                // indent level control
+                switch (object) {
+                    case '(':
+                    case '[':
+                        indent_level++; 
+                        break;
 
-            // indent level control
-            switch (object) {
-                case '(':
-                case '[':
-                    indent_level++; 
-                    break;
+                    case ')':
+                    case ']':
+                        indent_level--;
+                        break;
 
-                case ')':
-                case ']':
-                    indent_level--;
-                    break;
+                    default:
+                        break;
+                }
 
-                default:
-                    break;
-            }
+                // Add formatting(indentation and new lines) to the text
+                switch (object) {
+                    case '(':
+                    case '[':
+                    case ';':
+                        return object + '\n' + tab.repeat(indent_level);
+                        break;
+                    
+                    case ')':
+                    case ']':
+                        return '\n' + tab.repeat(indent_level) + object;
+                        break;
 
-            // Add to the text
-            switch (object) {
-                case '(':
-                case '[':
-                    return object + '\n' + tab.repeat(indent_level);
-                    break;
-                
-                case ')':
-                case ']':
-                    return '\n' + tab.repeat(indent_level) + object;
-                    break;
-
-                default:
-                    return object;
+                    default:
+                        return object;
+                }
             }
         };
         output_text = output_text + new_text(object)
     }
 
+
     //Add carriage returns back
     output_text = output_text.replace(/<<end_comment>>/g, '\n')
+
 
     // Update the DOM
     document.getElementById('output_text').innerHTML = output_text
